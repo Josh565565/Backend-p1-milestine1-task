cfa362685434b02a8044b0156d5d3b2775abb50f
Popsicool <akinolasamson1234@gmail.com>
2023-10-21 08:48:41 -0700

    update models and views

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ping, name="ping"),
+    path('search', views.MainObjectSearch.as_view(), name='main-object-search'),
+    path('api/main-objects/', views.MainObjectList.as_view(), name='main-object-list'),
+    path('api/main-objects/<int:pk>/', views.MainObjectDetail.as_view(), name='main-object-detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics, status
+from django.db.models import Q
+from .models import MainObject
+from .serializers import MainObjectSerializer
 # Create your views here.
 
 @swagger_auto_schema(method="GET",
@@ -10,3 +14,34 @@ from rest_framework.response import Response
 def ping(request):
     message = {"reply": "ping"}
     return Response(message)
+
+
+class MainObjectSearch(generics.ListAPIView):
+    serializer_class = MainObjectSerializer
+    queryset = MainObject.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        search_term = self.request.query_params.get('query', '')
+        if len(search_term) < 1:
+            return Response([], status=status.HTTP_200_OK)
+        keywords = search_term.split()
+        print(keywords)
+        q_objects = []
+
+        for keyword in keywords:
+            q_objects.append(Q(name__icontains=keyword) | Q(postcode__icontains=keyword))
+        query = q_objects.pop()
+        for item in q_objects:
+            query |= item
+        queryset = self.queryset.filter(query).distinct().order_by('-name')
+
+        serializer = MainObjectSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class MainObjectList(generics.ListCreateAPIView):
+    queryset = MainObject.objects.all()
+    serializer_class = MainObjectSerializer
+
+class MainObjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MainObject.objects.all()
+    serializer_class = MainObjectSerializer
